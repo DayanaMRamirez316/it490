f3b0bedbcccddb309246a71c6dab564a5b48aca1
Matthew Bono <mgb46@njit.edu>
2026-04-01 17:22:56 -0400

    Added/Modded logic along with tar archive file creation is complete. Need to add FTP and eventually rabbitMQ integration with deployment server

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,11 +2,22 @@
 cd ..
 find . \( -path "./.git" -o -path "./deployment" \) -prune -o -type f -exec sha256sum "{}" \; | sort > deployment/newVer.txt
 
-join -j 2 <(awk '{print $1, $2}' deployment/baseVer.txt | sort -k2,2) \
-          <(awk '{print $1, $2}' deployment/newVer.txt | sort -k2,2) \
+join -j 2 \
+    <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print $1, path }' deployment/baseVer.txt | sort -k2,2) \
+    <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print $1, path }' deployment/newVer.txt | sort -k2,2) \
 | awk '$2 != $3 {print $1}' > deployment/deploy.txt
 
-comm -13 <(awk '{print $2}' deployment/baseVer.txt | sort) \
-         <(awk '{print $2}' deployment/newVer.txt | sort) >> deployment/deploy.txt
+comm -13 \
+    <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print path }' deployment/baseVer.txt | sort) \
+    <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print path }' deployment/newVer.txt | sort) \
+>> deployment/deploy.txt
+
+sort -u deployment/deploy.txt -o deployment/deploy.txt
 
 tar -cvf deployment/update.tar -T deployment/deploy.txt
+
+# add ftp to send tar to deployement server
+
+mv deployment/newVer.txt deployment/baseVer.txt
+
+rm deployment/deploy.txt
