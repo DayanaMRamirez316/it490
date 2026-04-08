175e7fc7decffce4e9543dc4762f8f4fc24f2953
Mohsin Imtiaz <mi329@njit.edu>
2026-04-08 11:57:13 -0400

    Working on the rollback_trigger.php file Will add the Exception later.

--- rpc/deployment/bundles/scripts/rollerback_trigger.php
+++ rpc/deployment/bundles/scripts/rollerback_trigger.php
@@ -1,1 +1,14 @@
 <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini", "deploymentServer");
+
+$request = array();
+$request['type'] = "rollback";
+$request['version'] = "v1.0";
+$request['destination'] = "production";
+
+$response = $client->send_request($request);
+print_r($response);
