f415799f6ca68e5030698c321dd326e00308c3c6
Mohsin Imtiaz <mi329@njit.edu>
2026-04-12 16:55:43 -0400

    Added echo.

--- rpc/deployment/bundles/rollback.sh
+++ rpc/deployment/bundles/rollback.sh
@@ -6,12 +6,13 @@ PROD_VM="user@prod-ip"
 PROD_PATH="/var/www/html/"
 
 
-if [ -d "BUNDLE_PATH" ]; then
+if [ -d "$BUNDLE_PATH" ]; then
     echo "Reverting Production to $ROLLBACK_VER..."
 
     rsync  -avz --delete "$BUNDLE_PATH/" "$PROD_VM:$PROD_PATH"
 
     echo "Rollback to $ROLLBOCK_VER complete."
 else
-    echo "Error: Version $ROLLBACK_VER complete."
+    echo "Error: Version $ROLLBACK_VER wasn't found in $BUNDLE_PATH."
+    exit 1
 fi
