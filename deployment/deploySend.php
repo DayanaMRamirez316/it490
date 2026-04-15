8c1eae578dc64dc5fe74f61d84a87d10b86eb683
Matthew <mgb46@njit.edu>
2026-04-15 13:17:28 -0400

    testing routing key with deploy server

--- deployment/deploySend.php
+++ deployment/deploySend.php
@@ -17,7 +17,7 @@ else
 $request = array();
 $request['type'] = "deploy";
 $request['version'] = "1.0";
-$response = $client->send_request($request);
+$response = $client->publish($request, "dev");
 //$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
