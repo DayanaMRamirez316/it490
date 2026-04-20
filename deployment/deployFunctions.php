f932140d786f88ea7aaad3ac3fd46466a1245fcd
Matthew <mgb46@njit.edu>
2026-04-20 13:11:33 -0400

    started php intergration for scripting

---
+++ deployment/deployFunctions.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+
+function transmitPackage(rabbitMQClient $client) {
+  $request = array();
+  $request['type'] = "deploy";
+  $request['version'] = "1.0";
+  $response = $client->publish($request, "dev");
+}
+
+$client = new rabbitMQClient("deploy.ini","testServer");
+$command = strtolower($argv[1]);
+
+switch ($command) {
+  case "pack":
+    transmitPackage($client);
+}
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
