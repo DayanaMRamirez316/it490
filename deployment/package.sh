6c7acd88e3c1094c2dc9161d8d140d6c4fb6b6b5
Matthew <mgb46@njit.edu>
2026-04-29 13:13:04 -0400

    fixed script for rollback

--- deployment/package.sh
+++ deployment/package.sh
@@ -3,11 +3,11 @@ destDir="/opt/it490/"
 cd "$destDir"
 find . \( -path "./.git" -o -path "./deployment" \) -prune -o -type f -exec sha256sum --text "{}" \; | sort > ./deployment/newVer.txt
 
-if [[ "${1}" != "rollback" ]]; then
+if [[ "$1" != "rollback" ]]; then
     echo not rollback
     join -j 2 \
         <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print $1, path }' deployment/baseVer.txt | sort -k2,2) \
-        <(awk '{ path=substr($0, index($git 0,$2)); sub(/^\*/, "", path); print $1, path }' deployment/newVer.txt | sort -k2,2) \
+        <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print $1, path }' deployment/newVer.txt | sort -k2,2) \
     | awk '$2 != $3 {print $1}' > deployment/deploy.txt
     comm -13 \
         <(awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print path }' deployment/baseVer.txt | sort) \
@@ -15,16 +15,18 @@ if [[ "${1}" != "rollback" ]]; then
     >> deployment/deploy.txt
     sort -u deployment/deploy.txt -o deployment/deploy.txt
 else
-    sort -u deployment/newVer.txt -o deployment/deploy.txt
+    awk '{ path=substr($0, index($0,$2)); sub(/^\*/, "", path); print path }' deployment/newVer.txt \
+    | sort -u > deployment/deploy.txt
 fi
+
 awk '{ gsub(/^\.\//, "", $0); print "./"$0 "|" "/opt/it490/" $0 }' deployment/deploy.txt > deployment/manifest.txt
 
 if [ "${1}" = "rollback" ]; then
     filename="rollback.tar"
-    do_transfer= false
+    do_transfer=false
 else
     filename="version_${1}.tar"
-    do_transfer= true
+    do_transfer=true
 fi
 
 tar -cvf deployment/"$filename" -T deployment/deploy.txt deployment/manifest.txt
@@ -39,7 +41,11 @@ if [[ "$do_transfer" = true ]]; then
     bye
 EOF
 fi
-# metadata stuff
-mv deployment/newVer.txt deployment/baseVer.txt
+
+if [[ "$1" != "rollback" ]]; then
+    mv deployment/newVer.txt deployment/baseVer.txt
+else
+    rm -f deployment/newVer.txt
+fi
 
 rm -f deployment/deploy.txt
