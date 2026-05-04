1418a80db43df6ed3197e238eef6108a508c8f57
jbc526 <jbc5@njit.edu>
2026-05-04 09:16:09 -0400

    Clean up code on deployFunctions.php and created text file documentation on it

--- deployment/deployFunctions.php
+++ deployment/deployFunctions.php
@@ -10,6 +10,7 @@ require_once('rabbitMQLib.inc');
  * All functions use RMQ to send commands to the various clusters
  * 
  */
+//this will print the bashscript output for debugging
 function bashReport($output) {
   foreach ($output as $line) {
 		echo "$line \n";
@@ -18,7 +19,7 @@ function bashReport($output) {
 // Called upon for connection to mySQL database
 function dbConnect() {
   $mydb = new mysqli('127.0.0.1','deployer','deployPwd!','deployDB');
-  
+  //stop the script if database connection fails and then print the error
   if ($mydb->errno != 0) {
 	  echo "failed to connect to database: " . $mydb->error . PHP_EOL;
 	  exit(0);
@@ -38,46 +39,56 @@ function dbConnect() {
  * @return string[] | void Returns an array of strings, formatted in JSON
  */
 function sendPackage(rabbitMQClient $client, mysqli $mydb) {
+  //get the next deployment version number
   $query = "SELECT ROUND(MAX(version) + 0.01, 2) AS newVer FROM deployment_packages";
   $stmt = $mydb->prepare($query);
   $stmt->execute();
   $result = $stmt->get_result();
   $row = $result->fetch_assoc();
   $newVer = $row['newVer'];
+  //build the filename using the new version
   $filename = "version_$newVer.tar";
 
+  //create package request to send through RabbitMq
   $request = array();
   $request['type'] = "package";
   $request['version'] = "$newVer";
+  //send the package request to the VMs listening
   $response = $client->publish_to_exchange($request, "dev");
 
+  //save the new package entry in deployment database 
   $query = "INSERT INTO deployment_packages (packageName, version) VALUES (?, ?)";
   $stmt = $mydb->prepare($query);
   $stmt->bind_param('ss', $filename, $newVer);
   $stmt->execute();
-
+  //wait for the package to be created before allowing deploy
   sleep(2);
 
+  //combin parts of the package to be in one wrapped deployment archive
   exec("/bin/bash /opt/it490/deployment/deploy_wrap.sh wrap $newVer", $output, $code);
   bashReport($output);
   if ($code != 0) return;
   return $response;
 }
 
+//send deploy request for selected version and environment
 function sendDeploy(rabbitMQClient $client, $version, $env) {
-  
+  //unwrap the deployment archive before sending deploy request
   if ($version != "rollback") {
     exec("/bin/bash /opt/it490/deployment/deploy_wrap.sh unwrap $version", $output, $code);
     bashReport($output);
     if ($code != 0) return;
-  } else echo 'rollback: skipping unwrap';
+  } else echo 'rollback: skipping unwrap'; //skip unwrap if it is a rollback deployment 
+  //build the deploy request 
   $request = array();
   $request['type'] = "deploy";
   $request['version'] = "$version";
+  //send deploy request to selected environment
   $response = $client->publish_to_exchange($request, $env);
   return $response;
 }
 //store data to database
+//mark deployment version to as passed in database
 function markPass($mydb, $version){
 	//update deployment_packages
 	$query = "UPDATE deployment_packages SET status = 'passed' WHERE version = ?";
@@ -93,22 +104,27 @@ function markFail($mydb, $client, $version){
 	$stmt = $mydb->prepare($query);
 	$stmt->bind_param('s', $version);
 	$stmt->execute();
-
+  //send rollback request after marking deployment as failed
 	sendDeploy($client, "rollback", "dev"); //change env to "qa" later
 }
 
+//the command loop for deployment actions
 function run() {
+  //keep prompting until the user chooses to quit.
   $stop = false;
   while (!$stop) {
     global $argv, $mydb, $client;
+    //read user input 
     $input = readline("Enter a command: (enter -l for list of acceptable commands) ");
+    //make the command input case-insensitive and trim the dash for easier parsing
     $command = strtolower(ltrim($input, '-'));
 
     switch ($command) {
-      case "p": //pack
+      case "p": //package files from VMs 
         $response = sendPackage($client, $mydb);
         break;
-      case str_starts_with($command, "d"): //deploy
+      case str_starts_with($command, "d"): //deploy selected version to environment
+        //split the command into status and version
         $params = explode(" ", $command);
         if (count($params) != 3) {
           echo "Need 2 parameters: [version] [environment]";
@@ -141,10 +157,10 @@ function run() {
               -q  --  quit
              ".PHP_EOL;
         break;
-      case "q"; //quit
+      case "q"; //quit deployment command loop
         $stop = true;
         continue 2;
-      default: //catchall
+      default: //catch invalid commands
         echo "Enter a valid command please (-l for list of commands)";
         break;
     }
@@ -153,7 +169,7 @@ function run() {
 
 $mydb = dbConnect();
 $client = new rabbitMQClient("deploy.ini","testServer");
-
+//start the deployment command loop
 run();
 
 echo "Bye".PHP_EOL	;
